00e6b1e545117852ee91286e550fe3b654fb994b
Andrew Malcolm <andrew.malcolm@newvoicemedia.com>
2018-03-22 15:52:32 +0000

    new test for mongodb

--- src/mongodb/database_test.go
+++ src/mongodb/database_test.go
@@ -6,19 +6,22 @@ import (
 )
 
 // TestMain :
-func TestMain(m *testing.M) {
+// func TestMain(m *testing.M) {
 
-}
+// }
 
 // TestInsert :
 func TestInsert(t *testing.T) {
+
+	fmt.Println("TestInsert")
+
 	initDB()
 	defer closeDB()
 	deleteAllMessagesDB()
 
 	messages := queryAllMessagesDB()
 
-	if len(messages) != 1 {
+	if len(messages) != 0 {
 		t.Errorf("delete all failed")
 	}
 
@@ -27,6 +30,37 @@ func TestInsert(t *testing.T) {
 
 	if len(messages) != 1 {
 		t.Errorf("insert message failed")
+	}
+
+	fmt.Println("done")
+}
+
+func TestDelete(t *testing.T) {
+
+	fmt.Println("TestDelete")
+
+	initDB()
+	defer closeDB()
+	deleteAllMessagesDB()
+
+	messages := queryAllMessagesDB()
+
+	if len(messages) != 0 {
+		t.Errorf("delete all failed")
+	}
+
+	insertMessageDB(Message{Text: "Hello"})
+	messages = queryAllMessagesDB()
+
+	if len(messages) != 1 {
+		t.Errorf("insert message failed")
+	}
+
+	deleteMessageDB(messages[0].ID)
+	messages = queryAllMessagesDB()
+
+	if len(messages) != 0 {
+		t.Errorf("delete message failed")
 	}
 
 	fmt.Println("done")
