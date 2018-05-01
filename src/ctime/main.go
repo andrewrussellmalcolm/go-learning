715fe85d75cac3e22a4a1fd1b754fbb58c875654
Andrew Malcolm <arm@gurusystems.com>
2018-05-01 12:46:52 +0100

    opengl clock

--- src/ctime/main.go
+++ src/ctime/main.go
@@ -10,6 +10,10 @@ import (
 func main() {
 
 	fmt.Println(ctime.Strftime(time.Now(), "Full output %Y-%m-%d %H:%M:%S"))
+	fmt.Println(ctime.Strftime(time.Now(), "Full output alternate %Y-%b-%d %H:%M:%S"))
+
+	fmt.Println(ctime.Strftime(time.Now(), "Veeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeery looooooooooooooooooooooooooooooong %Y-%b-%d %H:%M:%S"))
+
 	fmt.Println(ctime.Strftime(time.Now(), "Today is %d/%m/%Y"))
 	fmt.Println(ctime.Strftime(time.Now(), "The time right now is %H:%M:%S"))
 }
