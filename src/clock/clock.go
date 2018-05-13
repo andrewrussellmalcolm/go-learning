6c3458b6f4941f7fb3e54a4335c6e6d5d8c4fcaa
Andrew Malcolm <andrewrussellmalcolm@gmail.com>
2018-05-13 08:34:19 +0100

    more custom drawing stuff

--- src/clock/clock.go
+++ src/clock/clock.go
@@ -53,8 +53,6 @@ func bailOnError(err error) {
 
 // DrawClock draws th c4 clock
 func DrawClock(w, h float64, ctx *cairo.Context) {
-	//fmt.Println("custom.Draw")
-
 	// calculate center and radius
 	x0 := w / 2
 	y0 := h / 2
@@ -86,10 +84,10 @@ func DrawClock(w, h float64, ctx *cairo.Context) {
 	ctx.SetSourceRGB(0, 0, 1.0)
 	ctx.SetLineWidth(16)
 	theta1 := -math.Pi * 2.0 * (hours + (mins / 60.0)) / 12.0
-	x3 := x0 + r*math.Sin(theta1)*0.75
-	y3 := y0 + r*math.Cos(theta1)*0.75
-	x4 := x0 - r*math.Sin(theta1)*0.2
-	y4 := y0 - r*math.Cos(theta1)*0.2
+	x3 := x0 - r*math.Sin(theta1)*0.75
+	y3 := y0 - r*math.Cos(theta1)*0.75
+	x4 := x0 + r*math.Sin(theta1)*0.2
+	y4 := y0 + r*math.Cos(theta1)*0.2
 	ctx.MoveTo(x4, y4)
 	ctx.LineTo(x3, y3)
 	ctx.Stroke()
@@ -98,10 +96,10 @@ func DrawClock(w, h float64, ctx *cairo.Context) {
 	ctx.SetSourceRGB(1.0, 0.0, 0.0)
 	ctx.SetLineWidth(10)
 	theta2 := -math.Pi * 2.0 * (mins + secs/60.0) / 60.0
-	x5 := x0 + r*math.Sin(theta2)*1.1
-	y5 := y0 + r*math.Cos(theta2)*1.1
-	x6 := x0 - r*math.Sin(theta2)*0.2
-	y6 := y0 - r*math.Cos(theta2)*0.2
+	x5 := x0 - r*math.Sin(theta2)*1.1
+	y5 := y0 - r*math.Cos(theta2)*1.1
+	x6 := x0 + r*math.Sin(theta2)*0.2
+	y6 := y0 + r*math.Cos(theta2)*0.2
 	ctx.MoveTo(x6, y6)
 	ctx.LineTo(x5, y5)
 	ctx.Stroke()
@@ -110,10 +108,10 @@ func DrawClock(w, h float64, ctx *cairo.Context) {
 	ctx.SetSourceRGB(1.0, 1.0, 0.0)
 	ctx.SetLineWidth(1)
 	theta3 := -math.Pi * 2.0 * secs / 60.0
-	x7 := x0 + r*math.Sin(theta3)*1.1
-	y7 := y0 + r*math.Cos(theta3)*1.1
-	x8 := x0 - r*math.Sin(theta3)*0.2
-	y8 := y0 - r*math.Cos(theta3)*0.2
+	x7 := x0 - r*math.Sin(theta3)*1.1
+	y7 := y0 - r*math.Cos(theta3)*1.1
+	x8 := x0 + r*math.Sin(theta3)*0.2
+	y8 := y0 + r*math.Cos(theta3)*0.2
 	ctx.MoveTo(x8, y8)
 	ctx.LineTo(x7, y7)
 	ctx.Stroke()
--- src/grid/grid.go
+++ src/grid/grid.go
@@ -1,45 +1,93 @@
 package main
 
-import "C"
+//go build -v -tags gtk_3_18 -gcflags "-N -l"
+
 import (
+	"fmt"
+	"os"
+
+	"github.com/gotk3/gotk3/gdk"
+
+	"github.com/gotk3/gotk3/cairo"
 	"github.com/gotk3/gotk3/gtk"
 )
 
 func main() {
 
-	gtk.Init(nil)
-
+	gtk.Init(&os.Args)
 	windowMain, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
 	bailOnError(err)
 
-	box, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0)
-	bailOnError(err)
-	windowMain.Container.Add(box)
-
 	windowMain.SetTitle("Controller")
 	windowMain.SetDefaultSize(600, 300)
-	windowMain.SetPosition(gtk.WIN_POS_CENTER)
+	//windowMain.SetPosition(gtk.WIN_POS_CENTER)
 	windowMain.Connect("destroy", func() {
 		gtk.MainQuit()
 	})
 
-	grid, err := gtk.GridNew()
-	bailOnError(err)
-	box.PackStart(grid, false, false, 0)
-
+	// some buttons
 	buttonStart, err := gtk.ButtonNewWithLabel("Start")
 	buttonStop, err := gtk.ButtonNewWithLabel("Stop")
 	buttonPanic, err := gtk.ButtonNewWithLabel("PANIC!!")
 
+	// custom
+	custom, err := CustomNew(DrawCustom)
+	custom.SetEvents(custom.GetEvents() | int(gdk.POINTER_MOTION_MASK) | int(gdk.BUTTON_PRESS_MASK))
+	custom.Connect("motion-notify-event", func(widget *gtk.DrawingArea, evt *gdk.Event) {
+
+		x, y := gdk.EventMotionNewFromEvent(evt).MotionVal()
+		fmt.Printf("MNE %f %f\n", x, y)
+	})
+
+	custom.Connect("button-press-event", func(widget *gtk.DrawingArea, evt *gdk.Event) {
+
+		x, y := gdk.EventButtonNewFromEvent(evt).MotionVal()
+		fmt.Printf("BPE %f %f\n", x, y)
+	})
+
+	// grid
+	grid, err := gtk.GridNew()
+	bailOnError(err)
 	grid.SetBorderWidth(10)
 	grid.SetRowSpacing(10)
 	grid.SetColumnSpacing(10)
 	grid.SetColumnHomogeneous(true)
 	grid.Attach(buttonStart, 0, 0, 1, 1)
-	grid.Attach(buttonStop, 3, 0, 1, 1)
-	grid.Attach(buttonPanic, 0, 1, 4, 1)
+	grid.Attach(buttonStop, 0, 1, 1, 1)
+	grid.Attach(buttonPanic, 0, 2, 1, 1)
 
-	grid.Attach(drawingArea, 2, 2, 3, 3)
+	// vertical split pane
+	pane, err := gtk.PanedNew(gtk.ORIENTATION_HORIZONTAL)
+	pane.SetBorderWidth(10)
+	pane.Add1(grid)
+	pane.Add2(custom)
+
+	windowMain.Add(pane)
+	windowMain.ShowAll()
 
 	gtk.Main()
+}
+
+// DrawCustom draws th c4 clock
+func DrawCustom(w, h float64, ctx *cairo.Context) {
+
+	ctx.SetSourceRGB(0, 0, 0)
+	ctx.Rectangle(0, 0, w, h)
+	ctx.Fill()
+
+	ctx.SetSourceRGB(1.0, 1.0, 1.0)
+	ctx.MoveTo(0, 0)
+	ctx.LineTo(w, h)
+	ctx.Stroke()
+
+	ctx.MoveTo(w, 0)
+	ctx.LineTo(0, h)
+	ctx.Stroke()
+
+}
+
+func bailOnError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
